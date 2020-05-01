4c58fe9ea380ed1e8e502a7420b7a79ac456c0f0
Adam H <adam.final@gmail.com>
2020-04-30 23:10:55 -0300

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+import { RiArrowDownSLine } from 'react-icons/ri';
+import { BsFillGearFill, BsFillInboxFill } from 'react-icons/bs';
+import { AiOutlineDashboard, AiFillPieChart } from 'react-icons/ai';
+import {
+  FaRegCalendarPlus,
+  FaRegCalendarAlt,
+  FaNetworkWired,
+  FaRegNewspaper,
+} from 'react-icons/fa';
+import { MdShowChart } from 'react-icons/md';
+import { Col, Row } from 'react-bootstrap';
+import { TopHeader, HeaderUi } from './styles';
+import logomlabs from '../../assets/logo.svg';
+
+const Header = () => {
+  return (
+    <>
+      <TopHeader fluid>
+        <Row className="align-items-center">
+          <Col className="clock">
+            <h4>30/04/2020 21:00</h4>
+          </Col>
+          <Col className="account">
+            <div>
+              <span>Minha conta</span>
+              <RiArrowDownSLine size={20} />
+            </div>
+          </Col>
+          <Col className="settings">
+            <BsFillGearFill size={20} />
+          </Col>
+        </Row>
+      </TopHeader>
+      <HeaderUi fluid>
+        <Row className="align-items-center">
+          <Col xs={12} sm={12} lg={4} xl={2}>
+            <img src={logomlabs} />
+          </Col>
+          <Col xs={12} sm={12} lg={8} xl={10}>
+            <div className="options">
+              <ul>
+                <li className="active">
+                  <a href="link">
+                    <AiOutlineDashboard size={32} />
+                    <span>DASHBOARD</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <FaRegCalendarPlus size={32} />
+                    <span>AGENDAR POST</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <FaRegCalendarAlt size={32} />
+                    <span>CALENDÁRIO</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <BsFillInboxFill size={32} />
+                    <span>INBOX</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <FaRegNewspaper size={32} />
+                    <span>FEED</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <FaNetworkWired size={32} />
+                    <span>WORKFLOW</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <MdShowChart size={32} />
+                    <span>ACOMPANHAMENTO</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="link">
+                    <AiFillPieChart size={32} />
+                    <span>RELATÓRIOS</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </HeaderUi>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+import { Container } from 'react-bootstrap';
+
+export const TopHeader = styled(Container)`
+  background-color: #3a3a3a;
+  padding: 8px 78px;
+
+  .clock {
+    h4 {
+      margin: 0;
+      font-size: 16px;
+      color: #cacbcc;
+    }
+  }
+
+  .account {
+    display: flex;
+    justify-content: center;
+
+    > div {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+
+      span {
+        color: #cacbcc;
+      }
+
+      svg {
+        color: #cacbcc;
+      }
+    }
+  }
+
+  .settings {
+    display: flex;
+    justify-content: flex-end;
+
+    svg {
+      color: #cacbcc;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const HeaderUi = styled(Container)`
+  background-color: #fff;
+  border-bottom: 2px solid #d1d2d2;
+  padding: 0px 78px;
+
+  img {
+    width: 100%;
+  }
+
+  .options {
+    display: block;
+    ul {
+      display: flex;
+      list-style: none;
+      justify-content: space-between;
+      li {
+        display: block;
+        a {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 24px 16px;
+          transition: all 0.3s;
+          text-decoration: none;
+
+          span {
+            color: #545454;
+            font-size: 15px;
+            font-weight: 400;
+            margin-top: 12px;
+          }
+
+          svg {
+            color: #acacac;
+          }
+
+          &:hover {
+            background-color: #eef0f0;
+          }
+        }
+      }
+
+      li.active {
+        a {
+          background-color: #eef0f0;
+          span {
+            color: #e92b4d;
+            font-weight: 700;
+          }
+          svg {
+            color: #767676;
+          }
+        }
+      }
+    }
+  }
+`;
